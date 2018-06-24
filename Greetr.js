df36b4484c9b1cc154a160e7215b69f1c3dfd725
isaaclem <lem.chiew.choon@gmail.com>
2018-06-24 21:07:33 +0800

    Added properties and methods

--- Greetr.js
+++ Greetr.js
@@ -4,9 +4,70 @@
     return new Greetr.init(firstName, lastName, language); //Prevent from using new keyword everytime
   }
 
+  let supportedLangs = ['en','cn'];
+
+  let greetings = {
+    en: 'Hello',
+    cn: '嗨'
+  };
+
+  let formalGreetings = {
+    en: 'Greetings',
+    cn: '您好'
+  };
+
+  let logMessages = {
+    en: 'Logged In',
+    cn: '登陆'
+  }
+
   //This is where all functions sit
   Greetr.prototype = {
+    fullName: () => `${this.firstName} ${this.lastName}`,
 
+    validate: () => {
+      if(!~supportedLangs.indexOf(this.language)){
+        throw "Invalid languages!";
+      }
+    },
+
+    greeting: () => `${greetings[this.language]} ${this.firstName}!`,
+
+    formalGreeting: () => `${formalGreetings[this.language]}, ${this.fullName()}`,
+
+    greet: formal => {
+      let msg = '';
+
+      if(formal) {
+        msg = this.formalGreeting();
+      }else{
+        msg = this.greeting();
+      }
+
+      if(console) {
+        console.log(msg);
+      }
+
+      // 'this' refer to the calling object at execution time
+      // Making method chainable
+      return this;
+    },
+
+    log: () => {
+      if(console){
+        console.log(`${logMessages[this.language]}: ${this.fullName()}`);
+
+        return this;
+      }
+    },
+
+    setLang: lang => {
+      this.language = lang;
+
+      this.validate(); //Making sure language is supported
+
+      return this;
+    }
   };
 
   Greetr.init = function(fName = '', lName = '', lang = 'en'){
